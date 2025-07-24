9d77730d652ef035976787b9ab25c879ad35663b
moffdoomai-sys <moffdoom.ai@gmail.com>
2025-07-24 23:03:27 +0000

    Remove all experimental features from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: { unoptimized: true },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
